0dd6f0b9ae10e518fcbf15115d67b79f3b532f48
byubraden <byubraden26@gmail.com>
2025-02-20 21:57:18 -0700

    added some more bootstrap

--- Mission06_Stevens/obj/Debug/net8.0/Mission06_Stevens.AssemblyInfo.cs
+++ Mission06_Stevens/obj/Debug/net8.0/Mission06_Stevens.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission06_Stevens")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2bfd3c83ac6c233b96f98a76d7d6a7b2d025109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97aa8b68e95195d5d33bf97bbc443ce659e9a4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission06_Stevens")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission06_Stevens")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
